91afeacaf5b74cbb61d9164f5e77e4f931a981b7
Aden Herold <aden.herold1@gmail.com>
2020-07-26 22:08:56 +1000

    Project: Reverted env variable changes to workflow config and serverless config
    Project: Added .env file to source control

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,10 @@
 import ApolloBoost from "apollo-boost";
 
 const server = new ApolloBoost({
-  uri: process.env.REACT_APP_API_URL,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_PROD_API_URL
+      : process.env.REACT_APP_API_URL,
 });
 
 export default server;
